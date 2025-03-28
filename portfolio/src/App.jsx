171bc07e0bcdd0bb1f1703c771d0be7fa53b6818
Nicholas Hickman <nicholas.hickman@startecon.fi>
2025-03-28 12:34:53 +0200

    - Building some more - Added About Me section - Working on Technologies section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,40 +1,278 @@
 ﻿import React from 'react';
 import Navbar from './navbar';
 import "./Main.css";
-import { Grid, Typography, Box, Stack } from '@mui/material';
+import { Grid, Typography, Button, Container, Card, CardMeida, CardActionArea, CardContent, Box } from '@mui/material';
 import content from './Typography.json';
 
 const App = () => {
+    const buttonStyle = {
+        color: '#fff',
+        fontSize: '20px',
+        padding: '10px 15px',
+        border: '1px solid #ff0000',
+        maxWidth: '25%',
+        '&:hover': {
+            backgroundColor: '#b71c1c',
+            border: '1px solid #fff',
+        }
+    };
+
     return (
-        <section id ="landing" sx={{ height: '100vh', width: '100vw', overflow: 'hidden' }}>
+        <>
             <Navbar />
-            {/* Introduction */}
-            <Grid container maxWidth= "xl" sx={{position: 'absolute', top: '40%', left: '10%', maxWidth: '50%'}}>
-                <Grid item>
-                    <Typography variant="h2" gutterBottom sx={{
-                    fontSize: { xs: '2rem', md: '4rem', lg: '4rem', xl: '5rem' },
-                    fontWeight: 'bold'}}>
-                    {content.greeting}
-                    </Typography>
-                </Grid>
-
-                <Grid item width="70%">
-                    <Typography variant="body1" sx={{
-                        fontSize: { xs: '1rem', md: '1.25rem', lg: '2rem', xl: '1.5rem' }, lineHeight: 1.4}}>
-                        {content.intro.intro1}
-                        {content.intro.intro2}
-                        {content.intro.intro3}
-                    </Typography>
-                </Grid>
-            </Grid>
-
-            <Grid container maxWidth="xl" sx={{justifyContent:"center",} }>
-                    <Grid item>
-                <img src={content.divider.path}/>
+            <section id='landing'>
+                <Container maxWidth='xxl' disableGutters
+                    sx={{
+                        minHeight: '100vh',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        pb:0,
+                    }}
+                >
+                    <Grid container
+                        sx={{
+                            maxWidth: '1400px',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Grid item lg={6} md={6} sm={12} xs={12}
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'flex-start',
+                                justifyContent: 'center',
+                            }}
+                        >
+                            <Typography sx={{ fontSize: '62px', fontWeight: 'bold', pb: 2 }} color="#FFF">
+                                {content.greeting}
+                            </Typography>
+                            <Typography sx={{ fontSize: '28px' }} color="#FFF">
+                                {content.intro1}
+                            </Typography>
+                            <Typography sx={{ fontSize: '20px' }} color="#808080">
+                                {content.intro2}
+                            </Typography>
+                            <Typography sx={{ fontSize: '20px', pb: 2 }} color="#808080">
+                                {content.intro3}
+                            </Typography>
+                            <Button sx={buttonStyle}>Contact Me</Button>
+                        </Grid>
+                        <Grid item lg={6} md={6} sm={12} xs={12}
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center'
+                            }}>
+                            <img
+                                alt="Nico"
+                                src="public/images/Nico.png"
+                                style={{
+                                    maxWidth: '65%',
+                                    height: 'auto'
+                                }} />
+                        </Grid>
+                        <Grid item sx={{mt:'10vh'} }>
+                            <img alt="Divider Line"
+                                src="public/images/Line1.png" />
+                        </Grid>
                     </Grid>
-                </Grid>
-        </section>
+                </Container>
+            </section>
+
+            <section id='aboutme'>
+                <Container
+                    maxWidth='xxl'
+                    disableGutters
+                    sx={{
+                        minHeight: '70vh',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        pt:0,
+                    }}
+                >
+                    <Grid container
+                        sx={{
+                            maxWidth: '1400px',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        {/* Title Section */}
+                        <Grid item xs={12}
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'flex-start',
+                                justifyContent: 'center',
+                                width: '100%',
+                            }}
+                        >
+                            <Typography sx={{ fontSize: '62px', fontWeight: 'bold', pb: 2 }} color="#FFF">
+                                {content.info.title}
+                            </Typography>
+                        </Grid>
+
+                        {/* Content Section*/}
+                        <Grid container
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'space-between',
+                                width: '100%',
+                                gap: 4,
+                            }}
+                        >
+                            <Grid item lg={5} md={6} sm={12} xs={12}
+                                sx={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'flex-start', 
+                                    justifyContent: 'center',
+                                    backgroundImage: 'url(/public/images/introductionbg.png)',
+                                    backgroundSize: 'cover',
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundPosition: 'center',
+                                    borderRadius: '24px',
+                                    minHeight: '500px',
+                                    p: 4, 
+                                }}
+                            >
+                                <Typography sx={{ fontSize: '34px', fontWeight: 'bold', pb: 2 }} color="#FFF">
+                                    {content.info.intro1}
+                                </Typography>
+                                <Typography sx={{ fontSize: '20px', pb: 2 }} color="#FFF">
+                                    {content.info.intro2}
+                                </Typography>
+                                <Typography sx={{ fontSize: '20px', pb: 2 }} color="#FFF">
+                                    {content.info.intro3}
+                                </Typography>
+                                <Typography sx={{ fontSize: '20px'}} color="#FFF">
+                                    {content.info.intro4}
+                                </Typography>
+                            </Grid>
+                            <Grid item lg={5} md={6} sm={12} xs={12}
+                                sx={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'flex-start',
+                                    justifyContent: 'center',
+                                    backgroundImage: 'url(/public/images/experience.png)',
+                                    backgroundSize: 'cover',
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundPosition: 'center',
+                                    borderRadius: '24px',
+                                    minHeight: '500px',
+                                    p: 4,
+                                }}
+                            >
+                                <Typography sx={{ fontSize: '34px', fontWeight: 'bold', pb: 2 }} color="#FFF">
+                                    {content.experience.title}
+                                </Typography>
+                                <Typography sx={{ fontSize: '22px', pb: 2 }} color="#FFF">
+                                    {content.experience.intro1}
+                                </Typography>
+                                <Typography sx={{ fontSize: '20px', pb: 2 }} color="#808080">
+                                    {content.experience.intro2}
+                                </Typography>
+                                <Typography sx={{ fontSize: '20px', pb: 2 }} color="#FFF">
+                                    {content.experience.intro3}
+                                </Typography>
+                                <Typography sx={{ fontSize: '20px' }} color="#FFF">
+                                    {content.experience.intro4}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </section>
+
+            <section id='technologies'>
+                <Container
+                    maxWidth='xxl'
+                    disableGutters
+                    sx={{
+                        minHeight: '10vh',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        pt: 20,
+                    }}
+                >
+                    <Grid container
+                        sx={{
+                            maxWidth: '1400px',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        {/* Title Section */}
+                        <Grid item xs={12}
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'flex-start',
+                                justifyContent: 'center',
+                                width: '100%',
+                            }}
+                        >
+                            <Typography sx={{ fontSize: '62px', fontWeight: 'bold', pb: 2 }} color="#FFF">
+                                {content.technology.title}
+                            </Typography>
+                        </Grid>
+
+                        {/* Content Section*/}
+                        <Grid container
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'space-between',
+                                width: '100%',
+                                gap: 4,
+                            }}
+                        >
+                            <Grid item lg={12} md={6} sm={12} xs={12}
+                                sx={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    borderRadius: '24px',
+                                    minHeight: '500px',
+                                    p: 4,
+                                }}
+                            >
+                                <Typography sx={{ fontSize: '26px', pb: 2 }} color="#FFF">
+                                    {content.technology.subtitle}
+                                </Typography>
+                            </Grid>
+                            <Grid item lg={5} md={6} sm={12} xs={12}
+                                sx={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'flex-start',
+                                    justifyContent: 'center',
+                                    borderRadius: '24px',
+                                    minHeight: '500px',
+                                    p: 4,
+                                }}
+                            >
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </section>
+
+
+        </>
     );
-    
 };
+
 export default App;
--- portfolio/src/navbar.jsx
+++ portfolio/src/navbar.jsx
@@ -1,36 +1,48 @@
 import React from "react";
-import "./navbar.css";
-import { ButtonGroup, Button } from '@mui/material';
+import { Box, ButtonGroup, Button, Typography } from '@mui/material';
+import content from './Typography.json';
 
 const Navbar = () => {
     const buttonStyle = {
         color: '#fff',
-        border: '1px solid #ff0000', // Initial border color (or whatever your current color is)
+        border: '1px solid #ff0000',
         '&:hover': {
-            backgroundColor: '#b71c1c', // darker red background on hover
-            border: '1px solid #fff', // deeper border color on hover
+            backgroundColor: '#b71c1c',
+            border: '1px solid #fff',
         }
     };
+
     return (
-        <nav className="navbar">
-            <div className="navbar-left">
-                <div className="label">
-                    <div className="text-wrapper">Portfolio by Nicholas Hickman</div>
-                </div>
-            </div>
-            <div className="navbar-right">
-                <ButtonGroup
-                    color="error"
-                    orientation="horizontal"
-                    size="lg"
-                    spacing={2}
-                >
-                    <Button sx={buttonStyle}>C.V</Button>
-                    <Button sx={buttonStyle}>Projects</Button>
-                    <Button sx={buttonStyle}>Resume</Button>
-                </ButtonGroup>
-            </div>
-        </nav>
+        <Box
+            component="nav"
+            sx={{
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                padding: '10px 20px',
+                backgroundColor: '#000', // or your preferred color
+                position: 'fixed', // keeps navbar at top
+                top: 0,
+                left: 0,
+                zIndex: 1000, // ensures navbar stays above other content
+                boxSizing: 'border-box' // includes padding in width calculation
+            }}
+        >
+            <Box>
+                <Typography sx={{ fontSize: '18px', color: '#fff'}}>{content.Navbar.portfolio}</Typography>
+            </Box>
+            <ButtonGroup
+                color="error"
+                orientation="horizontal"
+                size="large"
+                sx={{ gap: '2px' }}
+            >
+                <Button sx={buttonStyle}><Typography>{content.Navbar.cv}</Typography></Button>
+                <Button sx={buttonStyle}><Typography>{content.Navbar.projects}</Typography></Button>
+                <Button sx={buttonStyle}><Typography>{content.Navbar.resume}</Typography></Button>
+            </ButtonGroup>
+        </Box>
     );
 };
 
